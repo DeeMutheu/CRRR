aa82865b2a0ead2ee0c472dd58a6b8b7e81ba26e
MartMbithi <martdevelopers254@gmail.com>
2024-01-11 07:06:12 +0300

    Implement Login Page

--- views/login.php
+++ views/login.php
@@ -11,44 +11,53 @@ require_once('../partials/head.php');
         </div>
         <!-- Preloader end-->
 
-        <!-- Page Content -->
-        <div class="page-content">
-            <!-- Banner -->
-            <div class="banner-wrapper shape-1">
-                <div class="container inner-wrapper">
-                    <h2 class="dz-title">Collaborative Road Rescue Ready</h2>
-                    <br>
-                </div>
-            </div>
-            <!-- Banner End -->
-            <br><br><br><br><br><br>
-            <div class="container">
-                <h2 class="dz-title">Sign In</h2>
-                <p class="mb-0">Please sign in to your account</p><br><br>
-                <div class="account-area">
-                    <form class="">
-                        <div class="input-group">
-                            <input type="text" placeholder="User Name" class="form-control">
+        <!-- Welcome Start -->
+        <div class="content-body">
+            <div class="container vh-100">
+                <div class="welcome-area">
+                    <div class="welcome-logo">
+                        <img src="../assets/images/welcome/favicon.png" width="10%" alt="">
+                        <div class="get-started">
+                            <h1 class="dz-title">Collaborative Road Rescue Ready Application</h1>
+                            <br>
+                            <p align="justify">We're thrilled to have you on board for this exciting journey of collaboration and preparedness.
+                                Our mission is to create a safer and more connected road experience for everyone.
+                            </p>
                         </div>
-                        <div class="input-group">
-                            <input type="password" placeholder="Password" id="dz-password" class="form-control be-0">
-                            <span class="input-group-text show-pass">
-                                <i class="fa fa-eye-slash"></i>
-                                <i class="fa fa-eye"></i>
-                            </span>
+
+                        <div class="join-area">
+                            <div class="started">
+                                <h2>Login</h2>
+                                <hr>
+                            </div>
+                            <div class="account-area">
+                                <form class="">
+                                    <div class="input-group">
+                                        <input type="text" placeholder="Email address" name="login_email" class="form-control">
+                                    </div>
+                                    <div class="input-group">
+                                        <input type="password" placeholder="Password" name="login_password" id="dz-password" class="form-control be-0">
+                                        <span class="input-group-text show-pass">
+                                            <i class="fa fa-eye-slash"></i>
+                                            <i class="fa fa-eye"></i>
+                                        </span>
+                                    </div>
+                                    <a href="reset_password" class="btn-link d-block text-center">Forgot your password?</a>
+                                    <div class="input-group">
+                                        <button type="submit" name="login" class="btn mt-2 btn-primary w-100 btn-rounded">Login</button>
+                                    </div>
+                                    <div class="input-group">
+                                        <p class="text-center mb-0">Don't have an account? <a href="register">Sign Up</a></p>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <a href="reset_password" class="btn-link d-block text-center">Forgot your password?</a>
-                        <div class="input-group">
-                            <button type="submit" name="login" class="btn mt-2 btn-primary w-100 btn-rounded">Login</button>
-                        </div>
-                        <div class="input-group">
-                            <p class="text-center mb-0">Don't have an account? <a href="register">Sign Up</a></p>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- Page Content End -->
+        <!-- Welcome End -->
+
     </div>
     <!--**********************************
     Scripts
