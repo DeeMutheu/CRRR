411bd2f1ead7d595621347b30214535be94ee373
MartMbithi <martdevelopers254@gmail.com>
2024-01-11 10:20:57 +0300

    Implement Side Navigation Bar Pages

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -12,7 +12,7 @@
         <li class="nav-label">Main Menu</li>
 
         <li>
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="home">
                 <span class="dz-icon bg-pink light">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M9.14373 20.7821V17.7152C9.14372 16.9381 9.77567 16.3067 10.5584 16.3018H13.4326C14.2189 16.3018 14.8563 16.9346 14.8563 17.7152V20.7732C14.8562 21.4473 15.404 21.9951 16.0829 22H18.0438C18.9596 22.0023 19.8388 21.6428 20.4872 21.0007C21.1356 20.3586 21.5 19.4868 21.5 18.5775V9.86585C21.5 9.13139 21.1721 8.43471 20.6046 7.9635L13.943 2.67427C12.7785 1.74912 11.1154 1.77901 9.98539 2.74538L3.46701 7.9635C2.87274 8.42082 2.51755 9.11956 2.5 9.86585V18.5686C2.5 20.4637 4.04738 22 5.95617 22H7.87229C8.19917 22.0023 8.51349 21.8751 8.74547 21.6464C8.97746 21.4178 9.10793 21.1067 9.10792 20.7821H9.14373Z" fill="#130F26" />
@@ -22,7 +22,7 @@
             </a>
         </li>
         <li>
-            <a class="nav-link" href="pages.html">
+            <a class="nav-link" href="road_users">
                 <span class="dz-icon bg-orange light">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -36,7 +36,7 @@
             </a>
         </li>
         <li>
-            <a class="nav-link" href="ui-components.html">
+            <a class="nav-link" href="emergency_services">
                 <span class="dz-icon bg-skyblue light">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -50,7 +50,7 @@
             </a>
         </li>
         <li>
-            <a class="nav-link" href="notification.html">
+            <a class="nav-link" href="incidents">
                 <span class="dz-icon bg-green light">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -62,7 +62,7 @@
                 <span>Incidents</span>
             </a>
         </li>
-        <li><a class="nav-link" href="profile.html">
+        <li><a class="nav-link" href="profile">
                 <span class="dz-icon bg-yellow light">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -77,7 +77,7 @@
         </li>
 
         <li>
-            <a class="nav-link" href="onboading.html">
+            <a class="nav-link" href="../">
                 <span class="dz-icon bg-red light">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
