89b7592f98b14c5014d203146d5b6838f5de559d
MartMbithi <martdevelopers254@gmail.com>
2024-01-15 11:52:16 +0300

    Implement Side Navigation Bar

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,13 +1,10 @@
-<?php
-$users_sql = mysqli_query(
-    $mysqli,
-    "SELECT * FROM road_users WHERE user_login_id = '{$_SESSION['login_id']}'"
-);
-if (mysqli_num_rows($users_sql) > 0) {
-    while ($user = mysqli_fetch_array($users_sql)) {
-
-        if ($_SESSION['login_rank'] == 'Admin') {
-?>
+<?php if ($_SESSION['login_rank'] == 'Admin') {
+    $users_sql = mysqli_query(
+        $mysqli,
+        "SELECT * FROM road_users WHERE user_login_id = '{$_SESSION['login_id']}'"
+    );
+    if (mysqli_num_rows($users_sql) > 0) {
+        while ($user = mysqli_fetch_array($users_sql)) { ?>
             <div class="sidebar">
                 <div class="author-box">
                     <div class="dz-media">
@@ -119,91 +116,17 @@ if (mysqli_num_rows($users_sql) > 0) {
                     <p>App Version 1.0</p>
                 </div>
             </div>
-        <?php } else if ($_SESSION['login_rank'] == 'Response Service') { ?>
-            <div class="sidebar">
-                <div class="author-box">
-                    <div class="dz-media">
-                        <img src="../assets/images/user.png" class="rounded-circle" alt="author-image">
-                    </div>
-                    <div class="dz-info">
-                        <span><?php echo $greeting; ?></span>
-                        <h5 class="name"><?php echo $user['user_name']; ?></h5>
-                    </div>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="nav-label">Main Menu</li>
+        <?php }
+    }
+} else if ($_SESSION['login_rank'] == 'Road User') {
+    $users_sql = mysqli_query(
+        $mysqli,
+        "SELECT * FROM road_users WHERE user_login_id = '{$_SESSION['login_id']}'"
+    );
+    if (mysqli_num_rows($users_sql) > 0) {
+        while ($user = mysqli_fetch_array($users_sql)) {
+        ?>
 
-                    <li>
-                        <a class="nav-link" href="home">
-                            <span class="dz-icon bg-pink light">
-                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M9.14373 20.7821V17.7152C9.14372 16.9381 9.77567 16.3067 10.5584 16.3018H13.4326C14.2189 16.3018 14.8563 16.9346 14.8563 17.7152V20.7732C14.8562 21.4473 15.404 21.9951 16.0829 22H18.0438C18.9596 22.0023 19.8388 21.6428 20.4872 21.0007C21.1356 20.3586 21.5 19.4868 21.5 18.5775V9.86585C21.5 9.13139 21.1721 8.43471 20.6046 7.9635L13.943 2.67427C12.7785 1.74912 11.1154 1.77901 9.98539 2.74538L3.46701 7.9635C2.87274 8.42082 2.51755 9.11956 2.5 9.86585V18.5686C2.5 20.4637 4.04738 22 5.95617 22H7.87229C8.19917 22.0023 8.51349 21.8751 8.74547 21.6464C8.97746 21.4178 9.10793 21.1067 9.10792 20.7821H9.14373Z" fill="#130F26" />
-                                </svg>
-                            </span>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="incidents">
-                            <span class="dz-icon bg-green light">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <path d="M17,12 L18.5,12 C19.3284271,12 20,12.6715729 20,13.5 C20,14.3284271 19.3284271,15 18.5,15 L5.5,15 C4.67157288,15 4,14.3284271 4,13.5 C4,12.6715729 4.67157288,12 5.5,12 L7,12 L7.5582739,6.97553494 C7.80974924,4.71225688 9.72279394,3 12,3 C14.2772061,3 16.1902508,4.71225688 16.4417261,6.97553494 L17,12 Z" fill="#fff" />
-                                        <rect fill="#fff" opacity="0.3" x="10" y="16" width="4" height="4" rx="2" />
-                                    </g>
-                                </svg>
-                            </span>
-                            <span>Incidents</span>
-                        </a>
-                    </li>
-                    <li><a class="nav-link" href="profile">
-                            <span class="dz-icon bg-yellow light">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <polygon points="0 0 24 0 24 24 0 24" />
-                                        <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#fff" fill-rule="nonzero" opacity="0.3" />
-                                        <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#fff" fill-rule="nonzero" />
-                                    </g>
-                                </svg>
-                            </span>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a class="nav-link" href="../">
-                            <span class="dz-icon bg-red light">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <rect x="0" y="0" width="24" height="24" />
-                                        <path d="M14.0069431,7.00607258 C13.4546584,7.00607258 13.0069431,6.55855153 13.0069431,6.00650634 C13.0069431,5.45446114 13.4546584,5.00694009 14.0069431,5.00694009 L15.0069431,5.00694009 C17.2160821,5.00694009 19.0069431,6.7970243 19.0069431,9.00520507 L19.0069431,15.001735 C19.0069431,17.2099158 17.2160821,19 15.0069431,19 L3.00694311,19 C0.797804106,19 -0.993056895,17.2099158 -0.993056895,15.001735 L-0.993056895,8.99826498 C-0.993056895,6.7900842 0.797804106,5 3.00694311,5 L4.00694793,5 C4.55923268,5 5.00694793,5.44752105 5.00694793,5.99956624 C5.00694793,6.55161144 4.55923268,6.99913249 4.00694793,6.99913249 L3.00694311,6.99913249 C1.90237361,6.99913249 1.00694311,7.89417459 1.00694311,8.99826498 L1.00694311,15.001735 C1.00694311,16.1058254 1.90237361,17.0008675 3.00694311,17.0008675 L15.0069431,17.0008675 C16.1115126,17.0008675 17.0069431,16.1058254 17.0069431,15.001735 L17.0069431,9.00520507 C17.0069431,7.90111468 16.1115126,7.00607258 15.0069431,7.00607258 L14.0069431,7.00607258 Z" fill="#fff" fill-rule="nonzero" opacity="0.3" transform="translate(9.006943, 12.000000) scale(-1, 1) rotate(-90.000000) translate(-9.006943, -12.000000) " />
-                                        <rect fill="#ff4db8" opacity="0.3" transform="translate(14.000000, 12.000000) rotate(-270.000000) translate(-14.000000, -12.000000) " x="13" y="6" width="2" height="12" rx="1" />
-                                        <path d="M21.7928932,9.79289322 C22.1834175,9.40236893 22.8165825,9.40236893 23.2071068,9.79289322 C23.5976311,10.1834175 23.5976311,10.8165825 23.2071068,11.2071068 L20.2071068,14.2071068 C19.8165825,14.5976311 19.1834175,14.5976311 18.7928932,14.2071068 L15.7928932,11.2071068 C15.4023689,10.8165825 15.4023689,10.1834175 15.7928932,9.79289322 C16.1834175,9.40236893 16.8165825,9.40236893 17.2071068,9.79289322 L19.5,12.0857864 L21.7928932,9.79289322 Z" fill="#fff" fill-rule="nonzero" transform="translate(19.500000, 12.000000) rotate(-90.000000) translate(-19.500000, -12.000000) " />
-                                    </g>
-                                </svg>
-                            </span>
-                            <span>Logout</span>
-                        </a>
-                    </li>
-                    <li>
-                        <div class="mode">
-                            <span class="dz-icon bg-green light">
-                                <i class="fa-solid fa-moon"></i>
-                            </span>
-                            <span>Dark Mode</span>
-                            <div class="custom-switch">
-                                <input type="checkbox" class="switch-input theme-btn" id="toggle-dark-menu">
-                                <label class="custom-switch-label" for="toggle-dark-menu"></label>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-                <div class="sidebar-bottom">
-                    <h6 class="name">Collaborative Road Rescue Ready Application</h6>
-                    <p>App Version 1.0</p>
-                </div>
-            </div>
-        <?php } else { ?>
             <div class="sidebar">
                 <div class="author-box">
                     <div class="dz-media">
@@ -300,6 +223,99 @@ if (mysqli_num_rows($users_sql) > 0) {
                     <p>App Version 1.0</p>
                 </div>
             </div>
+        <?php }
+    }
+} else {
+    $users_sql = mysqli_query(
+        $mysqli,
+        "SELECT * FROM response_services WHERE response_service_login_id = '{$_SESSION['login_id']}'"
+    );
+    if (mysqli_num_rows($users_sql) > 0) {
+        while ($user = mysqli_fetch_array($users_sql)) { ?>
+            <div class="sidebar">
+                <div class="author-box">
+                    <div class="dz-media">
+                        <img src="../assets/images/user.png" class="rounded-circle" alt="author-image">
+                    </div>
+                    <div class="dz-info">
+                        <span><?php echo $greeting; ?></span>
+                        <h5 class="name"><?php echo $user['response_service_contact_person_name']; ?></h5>
+                    </div>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="nav-label">Main Menu</li>
+
+                    <li>
+                        <a class="nav-link" href="home">
+                            <span class="dz-icon bg-pink light">
+                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M9.14373 20.7821V17.7152C9.14372 16.9381 9.77567 16.3067 10.5584 16.3018H13.4326C14.2189 16.3018 14.8563 16.9346 14.8563 17.7152V20.7732C14.8562 21.4473 15.404 21.9951 16.0829 22H18.0438C18.9596 22.0023 19.8388 21.6428 20.4872 21.0007C21.1356 20.3586 21.5 19.4868 21.5 18.5775V9.86585C21.5 9.13139 21.1721 8.43471 20.6046 7.9635L13.943 2.67427C12.7785 1.74912 11.1154 1.77901 9.98539 2.74538L3.46701 7.9635C2.87274 8.42082 2.51755 9.11956 2.5 9.86585V18.5686C2.5 20.4637 4.04738 22 5.95617 22H7.87229C8.19917 22.0023 8.51349 21.8751 8.74547 21.6464C8.97746 21.4178 9.10793 21.1067 9.10792 20.7821H9.14373Z" fill="#130F26" />
+                                </svg>
+                            </span>
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="incidents">
+                            <span class="dz-icon bg-green light">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
+                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <path d="M17,12 L18.5,12 C19.3284271,12 20,12.6715729 20,13.5 C20,14.3284271 19.3284271,15 18.5,15 L5.5,15 C4.67157288,15 4,14.3284271 4,13.5 C4,12.6715729 4.67157288,12 5.5,12 L7,12 L7.5582739,6.97553494 C7.80974924,4.71225688 9.72279394,3 12,3 C14.2772061,3 16.1902508,4.71225688 16.4417261,6.97553494 L17,12 Z" fill="#fff" />
+                                        <rect fill="#fff" opacity="0.3" x="10" y="16" width="4" height="4" rx="2" />
+                                    </g>
+                                </svg>
+                            </span>
+                            <span>Incidents</span>
+                        </a>
+                    </li>
+                    <li><a class="nav-link" href="profile">
+                            <span class="dz-icon bg-yellow light">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
+                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <polygon points="0 0 24 0 24 24 0 24" />
+                                        <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#fff" fill-rule="nonzero" opacity="0.3" />
+                                        <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#fff" fill-rule="nonzero" />
+                                    </g>
+                                </svg>
+                            </span>
+                            <span>Profile</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="../">
+                            <span class="dz-icon bg-red light">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
+                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <rect x="0" y="0" width="24" height="24" />
+                                        <path d="M14.0069431,7.00607258 C13.4546584,7.00607258 13.0069431,6.55855153 13.0069431,6.00650634 C13.0069431,5.45446114 13.4546584,5.00694009 14.0069431,5.00694009 L15.0069431,5.00694009 C17.2160821,5.00694009 19.0069431,6.7970243 19.0069431,9.00520507 L19.0069431,15.001735 C19.0069431,17.2099158 17.2160821,19 15.0069431,19 L3.00694311,19 C0.797804106,19 -0.993056895,17.2099158 -0.993056895,15.001735 L-0.993056895,8.99826498 C-0.993056895,6.7900842 0.797804106,5 3.00694311,5 L4.00694793,5 C4.55923268,5 5.00694793,5.44752105 5.00694793,5.99956624 C5.00694793,6.55161144 4.55923268,6.99913249 4.00694793,6.99913249 L3.00694311,6.99913249 C1.90237361,6.99913249 1.00694311,7.89417459 1.00694311,8.99826498 L1.00694311,15.001735 C1.00694311,16.1058254 1.90237361,17.0008675 3.00694311,17.0008675 L15.0069431,17.0008675 C16.1115126,17.0008675 17.0069431,16.1058254 17.0069431,15.001735 L17.0069431,9.00520507 C17.0069431,7.90111468 16.1115126,7.00607258 15.0069431,7.00607258 L14.0069431,7.00607258 Z" fill="#fff" fill-rule="nonzero" opacity="0.3" transform="translate(9.006943, 12.000000) scale(-1, 1) rotate(-90.000000) translate(-9.006943, -12.000000) " />
+                                        <rect fill="#ff4db8" opacity="0.3" transform="translate(14.000000, 12.000000) rotate(-270.000000) translate(-14.000000, -12.000000) " x="13" y="6" width="2" height="12" rx="1" />
+                                        <path d="M21.7928932,9.79289322 C22.1834175,9.40236893 22.8165825,9.40236893 23.2071068,9.79289322 C23.5976311,10.1834175 23.5976311,10.8165825 23.2071068,11.2071068 L20.2071068,14.2071068 C19.8165825,14.5976311 19.1834175,14.5976311 18.7928932,14.2071068 L15.7928932,11.2071068 C15.4023689,10.8165825 15.4023689,10.1834175 15.7928932,9.79289322 C16.1834175,9.40236893 16.8165825,9.40236893 17.2071068,9.79289322 L19.5,12.0857864 L21.7928932,9.79289322 Z" fill="#fff" fill-rule="nonzero" transform="translate(19.500000, 12.000000) rotate(-90.000000) translate(-19.500000, -12.000000) " />
+                                    </g>
+                                </svg>
+                            </span>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="mode">
+                            <span class="dz-icon bg-green light">
+                                <i class="fa-solid fa-moon"></i>
+                            </span>
+                            <span>Dark Mode</span>
+                            <div class="custom-switch">
+                                <input type="checkbox" class="switch-input theme-btn" id="toggle-dark-menu">
+                                <label class="custom-switch-label" for="toggle-dark-menu"></label>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div class="sidebar-bottom">
+                    <h6 class="name">Collaborative Road Rescue Ready Application</h6>
+                    <p>App Version 1.0</p>
+                </div>
+            </div>
+
 <?php }
     }
 } ?>
